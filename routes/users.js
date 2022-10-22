f279a50298299ccad4b35e3fd17d1c194a4a8f55
thaina <thainabbraz@gmail.com>
2022-10-22 14:47:22 +0200

    auth token added

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
-
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 // Example:
 
 let posts = [
@@ -20,6 +21,15 @@ let posts = [
 
 router.get("/posts", (req, res) => {
   res.json(posts);
+});
+
+router.get("/login", (req, res) => {
+  let { username, password } = req.body;
+  let user = { name: username };
+
+  // Create token containing user name
+  let token = jwt.sign(user, process.env.SECRET_KEY);
+  res.json({ accessToken: token });
 });
 
 module.exports = router;
